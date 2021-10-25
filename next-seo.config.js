1b8f1ac9cf089e157dad82da17fc56500396b76b
ogtayhuseynov1 <ohuseynov2019@gmail.com>
2021-10-25 11:45:30 +0400

    seo change

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,25 +1,21 @@
 /** @type {import('next-seo').DefaultSeoProps} */
 const defaultSEOConfig = {
-  title: "nextarter-chakra",
-  titleTemplate: "%s | nextarter-chakra",
-  defaultTitle: "nextarter-chakra",
-  description: "Next.js + chakra-ui + TypeScript template",
+  title: "Todo - DevCave",
+  titleTemplate: "%s | Todo - DevCave",
+  defaultTitle: "Todo - DevCave",
+  description: "Todo - DevCave todo app",
   canonical: "https://nextarter-chakra.sznm.dev",
   openGraph: {
     url: "https://nextarter-chakra.sznm.dev",
-    title: "nextarter-chakra",
-    description: "Next.js + chakra-ui + TypeScript template",
+    title: "Todo - DevCave",
+    description: "\"Todo - DevCave todo app\"",
     images: [
       {
         url: "https://og-image.sznm.dev/**nextarter-chakra**.sznm.dev.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fsznm.dev%2Favataaars.svg&widths=250",
         alt: "nextarter-chakra.sznm.dev og-image",
       },
     ],
-    site_name: "nextarter-chakra",
-  },
-  twitter: {
-    handle: "@sozonome",
-    cardType: "summary_large_image",
+    site_name: "Todo - DevCave",
   },
 };
 
